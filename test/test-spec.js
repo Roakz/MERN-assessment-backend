997af6856f3c091402f54c85932c8dc9e70eebd5
Roarkz <rory_bell_88@hotmail.com>
2020-01-13 20:04:10 +1000

    added chai and chai http as development dependencies plus a few notes to the test file

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,10 +1,21 @@
-// This test file is purely for proof that mocha is installed and operational. 
+// This test file is purely for proof that mocha is installed and operational.
 // It can be deleted after writing real tests.
-var assert = require('assert');
+
+// require chai and the chai http plugin...this must be done if using chai or chai http assertions
+
+// Chai will be used for the assertion library with the mocha test framework to provide the describe/it
+//  and running of tests.
+
+const chai = require('chai'),
+	chaiHttp = require('chai-http')
+
+chai.use(chaiHttp)
+
+var assert = require('assert')
 describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', function() {
+			assert.equal([1, 2, 3].indexOf(4), -1)
+		})
+	})
+})
